334272fc520079c13813248548aa2689a268c3d2
pa-palagyij <112852384+pa-palagyij@users.noreply.github.com>
2022-09-06 14:31:09 +0200

    Add files via upload

---
+++ dal.py
@@ -0,0 +1,6 @@
+print("Hello world/
+
+Arcomba temetett kézzel leplezem, hogy szenvedek
+Ha kinyitom a szemem reggel, tudom, hogy több kell neked.
+Megértettem, elfogadtam, jól van ez így,
+Hiába nincs is annál szebb, ha együtt dobban a szív.")
